806a1d2c3a8d91fd38de349a49a15aa4930c3b08
JuanSobron01 <sobronjuancruz@gmail.com>
2023-10-30 21:18:39 -0300

    first commit

---
+++ src/main/java/com/example/projectjsobron/ProjectJsobronPooApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectjsobron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJsobronPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJsobronPooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectjsobron/ProjectJsobronPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectjsobron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJsobronPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
